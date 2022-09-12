98c63067925ba004a037f5f71e4acf738f1997e6
Harun Gachanja Gitundu <arrotechdesign@gmail.com>
2022-09-12 13:53:44 +0300

    add port env variable

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ def home():
     return redirect(url_for('blueprint_v1.index', _external=True))
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
